da180690c8879b0bfcbd13f2a246ed3ab5da18ac
金文东 <bowen.jin@vipshop.com>
2016-07-22 15:50:07 +0800

    environment requst

---
+++ rpc-common/src/main/java/com/bowen/natie/rpc/common/EnvRequest.java
@@ -0,0 +1,25 @@
+package com.bowen.natie.rpc.common;
+
+/**
+ * Created by bowen.jin on 2016-7-22.
+ */
+public class EnvRequest {
+    private String host;
+    private String sign;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
