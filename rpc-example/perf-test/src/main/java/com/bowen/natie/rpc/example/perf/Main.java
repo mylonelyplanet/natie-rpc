151dcd44bd2372b88829925a5a4c985fc3ee82d3
金文东 <bowen.jin@vipshop.com>
2016-10-31 18:44:35 +0800

    test

--- rpc-example/perf-test/src/main/java/com/bowen/natie/rpc/example/perf/Main.java
+++ rpc-example/perf-test/src/main/java/com/bowen/natie/rpc/example/perf/Main.java
@@ -9,9 +9,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.concurrent.ScheduledExecutorTask;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by mylonelyplanet on 16/8/22.
@@ -20,28 +25,41 @@ import java.util.List;
 public class Main {
 
     private final static Logger logger = LoggerFactory.getLogger(Main.class);
+    private static ApplicationContext ctx;
 
     public static void main(String[] args) throws Exception{
         List<Object> apps = new ArrayList();
         apps.add(StartProxy.class);
         apps.add(Main.class);
+        ApplicationContext ctx = SpringApplication.run(apps.toArray(), args);
 
-        ApplicationContext ctx = SpringApplication.run(apps.toArray(),args);
-
-        RpcProxy helper = ctx.getBean(RpcProxy.class);
+        ExecutorService exec = Executors.newCachedThreadPool();
 
         logger.info("start test....... ");
         long start_time = System.currentTimeMillis();
+        for(int i =0; i < 100; i ++){
+            final int num = i;
+            Callable<String> task = new Callable<String>() {
+                @Override
+                public String call() throws Exception {
 
-        for (int i = 0; i < 10; i++) {
-
-            Greeting serviceHelper = helper.create(Greeting.class);
-            EnvRequest env = new EnvRequest();
-            serviceHelper.hello("baby", env);
-
+                    RpcProxy helper = ctx.getBean(RpcProxy.class);
+                    Greeting serviceHelper = helper.create(Greeting.class);
+                    EnvRequest env = new EnvRequest();
+                    Thread.currentThread().sleep(1000);
+                    String result = serviceHelper.hello("baby" + num, env);
+                    System.out.println("运行结果为:" + result);
+                    return result;
+                }
+            };
+            System.out.println("子进程正在运行: " +num);
+            exec.submit(task);
         }
+
         long end_time = System.currentTimeMillis();
         logger.info("end test....... ");
-        System.out.println(end_time-start_time);
+        System.out.println(end_time - start_time);
+        exec.shutdown();
     }
+
 }
